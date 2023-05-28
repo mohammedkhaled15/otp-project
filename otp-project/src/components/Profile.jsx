d76fe4915d3c82ee4e57d2c741fef72e5c92a2f2
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-05-29 00:57:24 +0300

    ..

--- otp-project/src/components/Profile.jsx
+++ otp-project/src/components/Profile.jsx
@@ -1,8 +1,10 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import usePrivateRequest from "../hooks/usePrivateRequest"
 import LoginAuthContext from "../context/LoginAuthProvider"
 
 const Profile = () => {
+
+  const [dummyData, setDummyData] = useState("")
 
   const { auth } = useContext(LoginAuthContext)
   console.log(auth)
@@ -10,12 +12,14 @@ const Profile = () => {
 
   const getUserData = async () => {
     const res = await privateRequest.post("/user/details")
-    console.log(res)
+    setDummyData(res?.data?.data)
+    console.log(res.data?.data)
   }
   return (
     <div>
       <h1>Profile</h1>
       <button onClick={getUserData}>Get User Data</button>
+      {dummyData && <h5>{JSON.stringify(dummyData)}</h5>}
     </div>
   )
 }
