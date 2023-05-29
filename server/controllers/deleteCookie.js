2ce71e9f4878c9938ac4267e4635881e2ccf04e1
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-05-29 11:10:29 +0300

    ..

--- server/controllers/deleteCookie.js
+++ server/controllers/deleteCookie.js
@@ -1,9 +1,9 @@
 const deleteCookie = async (req, res) => {
-  res.cookie("access_token", "none", {
+  res.cookie("access_token", undefined, {
     expires: new Date(Date.now() + 2 * 1000),
     httpOnly: true,
   });
-  res.status(204);
+  res.status(204).end();
 };
 
 module.exports = deleteCookie;
