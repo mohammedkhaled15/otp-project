46033c64ff3fdd02701c6f0912fc537274561f9f
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-05-31 01:44:23 +0300

    ..

--- server/controllers/sendToDb.js
+++ server/controllers/sendToDb.js
@@ -1,21 +1,35 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const sendToDb = async (req, res) => {
-  const userData = req?.body;
+  const { telephone, code, access_token } = req?.body;
   try {
     const alreadyExistUser = await User.findOne({
-      telephone: userData.telephone,
+      telephone,
     });
+
     if (!alreadyExistUser) {
-      await User.create({ ...userData });
+      await User.create({ telephone, code, access_token });
       return res.status(201).json({ message: `New User Data Created` });
     } else {
       await User.findByIdAndUpdate(
         { _id: alreadyExistUser._id },
-        { ...userData }
+        { telephone, code, access_token }
       );
     }
-    return res.status(201).json({ message: `User Data Updated` });
+
+    const serverAccessToken = jwt.sign(
+      { telephone, code },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    return res.status(200).json({
+      message: `User Data Updated`,
+      authData: { telephone, code, serverAccessToken },
+    });
   } catch (error) {
     console.log(error);
   }
