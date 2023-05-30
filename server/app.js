a78074f39a0ac4a21fe4b22a917c0d53b852dccd
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-05-30 08:42:20 +0300

    ..

--- server/app.js
+++ server/app.js
@@ -4,13 +4,13 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/authRouter");
-const credentials = require("./middleware/credentials");
+// const credentials = require("./middleware/credentials");
 
 connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(credentials());
+// app.use(credentials());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
