4f272fbd3d490b838a3217f1959a2864be10578b
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-05-31 15:21:30 +0300

    modify check/code request

--- server/controllers/sendToDb.js
+++ server/controllers/sendToDb.js
@@ -1,37 +1,47 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const axios = require("axios");
 
 const sendToDb = async (req, res) => {
   const { telephone, code, access_token } = req?.body;
-  try {
-    const alreadyExistUser = await User.findOne({
+  const chkRes = await axios.post(
+    `https://apis.refon-loyalty.com/api/check/code`,
+    {
       telephone,
-    });
-
-    if (!alreadyExistUser) {
-      await User.create({ telephone, code, access_token });
-      return res.status(201).json({ message: `New User Data Created` });
-    } else {
-      await User.findByIdAndUpdate(
-        { _id: alreadyExistUser._id },
-        { telephone, code, access_token }
-      );
+      code,
     }
+  );
+  if (chkRes.status === 200) {
+    try {
+      const alreadyExistUser = await User.findOne({
+        telephone,
+      });
 
-    const serverAccessToken = jwt.sign(
-      { telephone, code },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "7d",
+      if (!alreadyExistUser) {
+        await User.create({ telephone, code, access_token });
+        return res.status(201).json({ message: `New User Data Created` });
+      } else {
+        await User.findByIdAndUpdate(
+          { _id: alreadyExistUser._id },
+          { telephone, code, access_token }
+        );
       }
-    );
 
-    return res.status(200).json({
-      message: `User Data Updated`,
-      authData: { telephone, code, serverAccessToken },
-    });
-  } catch (error) {
-    console.log(error);
+      const serverAccessToken = jwt.sign(
+        { telephone, code },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "7d",
+        }
+      );
+
+      return res.status(200).json({
+        message: `User Data Updated`,
+        authData: { telephone, code, serverAccessToken },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 module.exports = sendToDb;
