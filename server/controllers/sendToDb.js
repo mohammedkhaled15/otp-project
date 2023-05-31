06c8d27856302573d094c9837ac6f0c4a678ecdc
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-05-31 15:39:08 +0300

    ..

--- server/controllers/sendToDb.js
+++ server/controllers/sendToDb.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const axios = require("axios");
 
 const sendToDb = async (req, res) => {
-  const { telephone, code, access_token } = req?.body;
+  const { telephone, code } = req?.body;
   const chkRes = await axios.post(
     `https://apis.refon-loyalty.com/api/check/code`,
     {
@@ -12,15 +12,14 @@ const sendToDb = async (req, res) => {
     }
   );
   if (chkRes.status === 200) {
+    const { name, telephone, access_token } = chkRes.data.data;
     try {
       const alreadyExistUser = await User.findOne({
         telephone,
       });
 
       if (!alreadyExistUser) {
-        await User.create({ telephone, code, access_token });
-        return res.status(201).json({ message: `New User Data Created` });
-      } else if (alreadyExistUser) {
+        await User.create({ telephone, name, access_token });
         const serverAccessToken = jwt.sign(
           { telephone, code },
           process.env.JWT_SECRET,
@@ -28,27 +27,30 @@ const sendToDb = async (req, res) => {
             expiresIn: "7d",
           }
         );
-      } else {
+        return res.status(201).json({
+          message: `New User Data Created`,
+          authData: { telephone, code, serverAccessToken },
+        });
+      } else if (alreadyExistUser) {
         await User.findByIdAndUpdate(
           { _id: alreadyExistUser._id },
-          { telephone, code, access_token }
+          { telephone, name, access_token }
         );
+        const serverAccessToken = jwt.sign(
+          { telephone, code },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "7d",
+          }
+        );
+        return res.status(200).json({
+          message: `User Data Updated`,
+          authData: { telephone, code, serverAccessToken },
+        });
       }
-
-      const serverAccessToken = jwt.sign(
-        { telephone, code },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: "7d",
-        }
-      );
-
-      return res.status(200).json({
-        message: `User Data Updated`,
-        authData: { telephone, code, serverAccessToken },
-      });
     } catch (error) {
       console.log(error);
+      return res.status(500).json({ message: "Unexpected Error from server" });
     }
   }
 };
